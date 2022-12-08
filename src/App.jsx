1af1881485800d0633a0fa874d2f0524da365abb
alnickelzon <al.villanueva1234@gmail.com>
2022-12-08 18:03:28 +0800

    depedency -> dependency

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   const [trigger, setTrigger] = useState(false);
   const [number, setNumber] = useState(0);
 
-  const myCallback = () => true; //when the App component rerenders, it returns a new render of myCallback function. In this code, we passed the myCallback as a prop to a child component. In that child component, myCallback is a depedency of an useEffect to check how myCallback changes when we rerender the App component.
+  const myCallback = () => true; //when the App component rerenders, it returns a new render of myCallback function. In this code, we passed the myCallback as a prop to a child component. In that child component, myCallback is a dependency of an useEffect to check how myCallback changes when we rerender the App component.
 
   // const myCallback = useCallback(() => true, [trigger]);
 
